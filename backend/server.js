fc6621931d939a647291029d7850f0cbc85480eb
sumeet.rana <sumeetsinh28@gmail.com>
2022-12-08 00:48:32 +0530

    dynamically adding routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
-const express = require("express");
 const cors = require("cors");
+const express = require("express");
+const { readdirSync } = require("fs");
+
 const app = express();
+
 app.use(cors());
-app.get("/", (req, res) => {
-  res.send("welcome from home");
-});
-app.get("/books", (req, res) => {
-  res.send("hahahahahahahhahahaaiidhiagduogauodhguagdigaiduygiuagduagdiu");
-});
-app.listen(8000, () => {
+
+// dynamically adds all the routes from routes folder 
+readdirSync('./routes').map(file => app.use("/", require("./routes/" + file)));
+
+app.listen(6060, () => {
   console.log("server is lestining...");
 });
