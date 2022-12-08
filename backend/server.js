4929018cdfc8acbff9d592adedbd7c5586b7091a
sumeet.rana <sumeetsinh28@gmail.com>
2022-12-08 22:02:46 +0530

    mongodb database connected

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const cors = require("cors");
+const dotenv = require("dotenv");
 const express = require("express");
+const mongoose = require("mongoose");
 const { readdirSync } = require("fs");
 
 const app = express();
+dotenv.config();
 
+// middlewares
 app.use(cors());
+app.use(express.json());
 
+// database
+mongoose.connect(process.env.DB_URL)
+  .then(() => console.log("Databse connected successfully.."))
+  .catch((err) => console.log(err))
+
+// routes
 // dynamically adds all the routes from routes folder 
 readdirSync('./routes').map(file => app.use("/", require("./routes/" + file)));
 
-app.listen(6060, () => {
-  console.log("server is lestining...");
+app.listen(process.env.PORT || 6060, () => {
+  console.log("server is running...");
 });
