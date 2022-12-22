7c9fed6ec48f8eef57cd52c5f7f9cf374f617707
sumeet.rana <sumeetsinh28@gmail.com>
2022-12-22 09:52:11 +0530

    user model

---
+++ backend/models/User.js
@@ -0,0 +1,138 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: [true, "firstname is required"],
+    trim: true,
+    text: true
+  },
+  last_name: {
+    type: String,
+    required: [true, "lastname is required"],
+    trim: true,
+    text: true,
+  },
+  username: {
+    type: String,
+    required: [true, "username is required"],
+    trim: true,
+    text: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+    trim: true
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  picture: {
+    type: String,
+    trim: true,
+    default: "https://i.pinimg.com/736x/7b/c6/fd/7bc6fd1adc9df8f49353a40d716a0a7d.jpg"
+  },
+  cover: {
+    type: String,
+    trim: true
+  },
+  gender: {
+    type: String,
+    required: [true, "gender is required"],
+    trim: true
+  },
+  bYear: {
+    type: Number,
+    required: true,
+    trim: true
+  },
+  bMonth: {
+    type: Number,
+    required: true,
+    trim: true
+  },
+  bDay: {
+    type: Number,
+    required: true,
+    trim: true
+  },
+  verified: {
+    type: Boolean,
+    default: false
+  },
+  friends: {
+    type: Array,
+    default: []
+  },
+  following: {
+    type: Array,
+    default: []
+  },
+  followers: {
+    type: Array,
+    default: []
+  },
+  requests: {
+    type: Array,
+    default: []
+  },
+  search: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    }
+  ],
+  details: {
+    bio: {
+      type: String
+    },
+    otherName: {
+      type: String,
+    },
+    job: {
+      type: String,
+    },
+    workplace: {
+      type: String,
+    },
+    highschool: {
+      type: String,
+    },
+    college: {
+      type: String,
+    },
+    currentCity: {
+      type: String,
+    },
+    hometown: {
+      type: String,
+    },
+    relationship: {
+      type: String,
+      enum: ["Single", "In a relationship", "Married"]
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  savedPosts: [
+    {
+      post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+      },
+      savedAt: {
+        type: Date,
+        default: new Date()
+      }
+    }
+  ]
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model("User", userSchema);
