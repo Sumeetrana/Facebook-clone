c89680f54f9c6f7c129f612c1c4b5ddf4e3511ed
sumeet.rana <sumeetsinh28@gmail.com>
2022-12-22 21:23:53 +0530

    backend: user login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { userRegistration, activateAccount } = require('../controllers/user');
+const { userRegistration, activateAccount, login } = require('../controllers/user');
 
 const router = express.Router();
 
 router.post("/register", userRegistration);
 router.post("/activate", activateAccount);
+router.post("/login", login);
 
 module.exports = router;
