7f7e7dc6d9bb76d4da0bc763c90f00480efe80b3
sumeet.rana <sumeetsinh28@gmail.com>
2022-12-27 17:13:49 +0530

    signup form ui

---
+++ frontend/src/components/inputs/registerInput/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ErrorMessage, useField } from 'formik';
+
+// import "./style.css";
+
+const RegisterInput = ({ placeholder, bottom, ...props }) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <div className='input_wrap'>
+      {meta.touched && meta.error && !bottom && <div className='input_error'>
+        <ErrorMessage name={field.name} />
+        <div className='error_arrow_top'></div>
+      </div>}
+      <input
+        className={meta.touched && meta.error ? "input_error_border" : ""}
+        type={field.type}
+        name={field.name}
+        placeholder={placeholder}
+        {...field}
+        {...props} />
+      {meta.touched && meta.error && bottom && <div className='input_error'>
+        <ErrorMessage name={field.name} />
+        <div className='error_arrow_bottom'></div>
+
+      </div>}
+      {meta.touched && meta.error && <i style={{ top: `${!bottom && "63%"}` }} className='error_icon'></i>}
+    </div>
+  );
+};
+
+export default RegisterInput;
