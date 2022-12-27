2b92b903262fb3f41d788cc7c4ac43fd7bfe0e7d
sumeet.rana <sumeetsinh28@gmail.com>
2022-12-27 14:58:32 +0530

    Login form design and formik integration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,14 @@ import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
 
-import { BrowserRouter as Router } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, composeWithDevTools);
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Formik, Form } from "formik";
+import { Link } from 'react-router-dom';
+
+import "./style.css";
+import LoginInput from '../../components/inputs/loginInput';
+
+const loginInfo = {
+  email: "",
+  password: ""
+}
 
 const Login = () => {
+  const [login, setLogin] = useState(loginInfo);
+  const { email, password } = login
+
+  const handleLoginChange = (e) => {
+    const { name, value } = e.target;
+    setLogin({
+      ...login,
+      [name]: value
+    })
+  }
+
   return (
-    <div>
-      Login
+    <div className='login'>
+      <div className='login_1_wrap'>
+        <div className='login_1'>
+          <img src="../../icons/facebook.svg" alt="" />
+          <span>
+            Facebook helps you connect and share with the people in your life.
+          </span>
+        </div>
+        <div className='login_1'>
+          <div className='login_2_wrap'>
+            <Formik
+              initialValues={{
+                email,
+                password
+              }}
+            >
+              {(formik) => (
+                <Form>
+                  <LoginInput
+                    placeholder="Email address or phone number"
+                    type="text"
+                    name="email"
+                    onChange={handleLoginChange}
+                  />
+                  <LoginInput
+                    placeholder="Password"
+                    type="password"
+                    name="password"
+                    onChange={handleLoginChange}
+                  />
+                  <button type="submit" className='blue_btn'>Log in</button>
+                </Form>
+              )}
+            </Formik>
+            <Link to="/forgot" className='forgot_password'>Forgot your password?</Link>
+            <div className='sign_splitter'></div>
+            <button className='blue_btn open_signup'>Create account</button>
+          </div>
+          <Link to="/">
+            <b>Create a Page</b>
+            for a celebrite, brand or business
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
